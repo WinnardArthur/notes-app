d3bc393800cd26a56f70471adf5dad0a11f2bf00
WinnardArthur <winnardarthur7@gmail.com>
2022-04-01 20:24:08 +0000

    feat: Add production code

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,19 @@ const uploadRoutes = require('./routes/fileUpload');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-}).then(console.log('Local Database running'))
+if (process.env.NODE_ENV === "production") {
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    }).then(console.log('Online Database running'))
+} else {
+    mongoose.connect(process.env.MONGO_URI_DEV, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    }).then(console.log('Local Database running'))
+}
 
 app.use(express.json())
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
@@ -21,6 +28,26 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use('/api', uploadRoutes)
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
+
+
+// ========== DEPLOYMENT ==============
+
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname1 = path.resolve()
+
+    app.use(express.static(path.join(__dirname1, "/frontend/build")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send("API is running successfully.")
+    })
+}
+
+// ========== DEPLOYMENT ==============
 
 
 app.use(notFound)
